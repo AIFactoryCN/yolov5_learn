ff1d9415028fadf9c6e9e3a1bd17ab73596a9f3b
AIFactoryCN <aifactory_1015@163.com>
2022-10-17 20:22:48 +0800

    add detect

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 import torch
 from tqdm import tqdm
+from models import heads
 from models.model import Model
 from dataloader import createDataLoader
 
-def run(model, test_loader, device=None):
+def run(model, head, test_loader, device=None):
     
-
+    
     for batch_index, (images, targets, paths, shapes) in enumerate(tqdm(test_loader)):
 
         images = images.to(torch.float32)
         targets = targets
 
-        predicts, _ = model(images)
+        predicts = model(images)
+        objs = head.detect(predicts)
+        
 
 
     
@@ -23,7 +26,8 @@ def run(model, test_loader, device=None):
 if __name__ == "__main__":
     model = Model("yamls/yolov5s.yaml")
     model.eval()
+    head = heads.Head()
     test_loader, dataSet = createDataLoader("/data/data_01/shituo/data/Mouse/mouse/test_list_learn.txt", 640, 2, False)
-    run(model, test_loader)
+    run(model, head, test_loader)
     
     
